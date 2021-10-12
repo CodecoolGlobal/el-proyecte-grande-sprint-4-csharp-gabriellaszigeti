d46ecfe7f18b477659c9d44a676a874fba74f1e2
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-12 15:14:46 +0200

    Refactor: Remove unnecessary/faulty changes from generated code.

--- el proyecte grande sprint 1/Models/IPictureStorage.cs
+++ el proyecte grande sprint 1/Models/IPictureStorage.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 public interface IPictureStorage {
 	IEnumerable<Picture> GetAllPictures();
-	void AddPicture(ref Picture picture);
-	void DeletePicture(ref Picture picture);
-	IEnumerable<Picture> GetAllPicturesByCategory(ref PictureCategory category);
-	IEnumerable<Picture> GetAllPicturesByUser(ref User username);
-	void AddNewComment(ref UUID pictureID, ref User username, ref Comment comment);
-	void IncreaseViews(ref UUID pictureID);
-	void IncreaseLikes(ref UUID pictureID);
-	void IncreaseDownloads(ref UUID pictureID);
+	void AddPicture(Picture picture);
+	void DeletePicture(Picture picture);
+	IEnumerable<Picture> GetAllPicturesByCategory(PictureCategory category);
+	IEnumerable<Picture> GetAllPicturesByUser(User username);
+	void AddNewComment(Guid pictureID,User username,Comment comment);
+	void IncreaseViews(Guid pictureID);
+	void IncreaseLikes(Guid pictureID);
+	void IncreaseDownloads(Guid pictureID);
 
 }
