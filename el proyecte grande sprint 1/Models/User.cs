cde424a8c2086957560837188a52587798f52850
gabriellaszigeti <gbszgt@gmail.com>
2021-10-12 13:46:21 +0100

    refactor : remove unneccessary code from User class

--- el proyecte grande sprint 1/Models/User.cs
+++ el proyecte grande sprint 1/Models/User.cs
@@ -1,19 +1,16 @@
 using System;
+using System.Security.Cryptography;
 
 public class User {
 	private string _username;
 	private Rfc2898DeriveBytes _password;
-	private UUID _userId;
+	private Guid _userId;
 
-	public Rfc2898DeriveBytes HashPassword(ref string password) {
+	public Rfc2898DeriveBytes HashPassword(string password) {
 		throw new System.NotImplementedException("Not implemented");
 	}
-	public void ChangePassword(ref string newPassword) {
+	public void ChangePassword(string newPassword) {
 		throw new System.NotImplementedException("Not implemented");
 	}
-
-	private Comment comment;
-
-	private UserStorage userStorage;
 
 }
