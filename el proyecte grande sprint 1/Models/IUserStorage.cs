0c26de3766de6f8098cc7e2154984b6ce9b7f8ce
gabriellaszigeti <gbszgt@gmail.com>
2021-10-12 13:46:07 +0100

    refactor : remove unneccessary code from IUserStorage interface

--- el proyecte grande sprint 1/Models/IUserStorage.cs
+++ el proyecte grande sprint 1/Models/IUserStorage.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 
 public interface IUserStorage {
-	void AddUser(ref User user);
+	void AddUser(User user);
 	IEnumerable<User> GetAllUsers();
-	User GetUserByUserId(ref UUID user_id);
-	User GetUserByUsername(ref string username);
+	User GetUserByUserId(Guid user_id);
+	User GetUserByUsername(string username);
 
 }
