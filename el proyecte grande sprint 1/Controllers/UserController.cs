be767fa93fa23e4c6547138462c2b559d8c9fcd0
gabriellaszigeti <gbszgt@gmail.com>
2021-10-13 13:11:41 +0100

    feat : add login page

--- el proyecte grande sprint 1/Controllers/UserController.cs
+++ el proyecte grande sprint 1/Controllers/UserController.cs
@@ -1,4 +1,26 @@
+using el_proyecte_grande_sprint_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+public class UserController : Controller
+{
 
-public class UserController {
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
 }
