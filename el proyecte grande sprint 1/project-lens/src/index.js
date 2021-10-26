fbb01c9e5db0c6e1f67f0e69d1ecddbce4d89342
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-10-26 14:06:58 +0200

    feat: Add MUI to the Project

--- el proyecte grande sprint 1/project-lens/src/index.js
+++ el proyecte grande sprint 1/project-lens/src/index.js
@@ -1,3 +1,11 @@
+//MUI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
