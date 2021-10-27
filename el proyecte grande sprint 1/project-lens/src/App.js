2cd4daf997cb3fd5f35d4ea03860c602759e749b
gabriellaszigeti <gbszgt@gmail.com>
2021-10-27 11:23:31 +0100

    feat : add  mainpage and container components to app.js

--- el proyecte grande sprint 1/project-lens/src/App.js
+++ el proyecte grande sprint 1/project-lens/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import MainPage from './components/MainPage';
+import Footer from './components/Footer';
+import background from "./components/spacex.jpg";
+import { Container } from 'reactstrap';
 
 function App() {
     return (
-        <Navbar>
-        </Navbar>
+        <Container style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', width:'100%' }}>
+            <Navbar>
+                </Navbar>
 
+            <MainPage>
+            </MainPage>
+            <Footer>
+                </Footer>
+            </Container>
   );
 }
 
