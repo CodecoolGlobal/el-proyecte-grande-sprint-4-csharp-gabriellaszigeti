4124110159117a39028bef9cbd2804f5ab47d86f
gabriellaszigeti <gbszgt@gmail.com>
2021-10-27 14:04:06 +0100

    feat : add routing for main page

--- el proyecte grande sprint 1/project-lens/src/App.js
+++ el proyecte grande sprint 1/project-lens/src/App.js
@@ -5,15 +5,19 @@ import MainPage from './components/MainPage';
 import background from "./components/spacex.jpg";
 import { Container } from 'reactstrap';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
-        <Container style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', width:'100%' }}>
-                <Navbar/>
-            <MainPage>
-            </MainPage>
-                <Footer/>
-            </Container>
-  );
+        <Container style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', width: '100%' }}>
+            <Navbar />
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={MainPage} exact />
+                </Switch>
+            </BrowserRouter>
+            <Footer />
+        </Container>
+    );
 }
 export default App;
