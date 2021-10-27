666c4f455bbffabbd8385d1de039cd7c6634039d
gabriellaszigeti <gbszgt@gmail.com>
2021-10-27 11:22:12 +0100

    feat: create mainpage

---
+++ el proyecte grande sprint 1/project-lens/src/components/MainPage.js
@@ -0,0 +1,27 @@
+﻿import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import SearchBar from './SearchBar';
+
+export default function SimpleContainer() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <Box sx={{ height: '85vh' }} style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                }} >
+                    <Button variant="contained" style={{ backgroundColor: 'gray', width: '40%', height: '10%' }}>Discover</Button>
+                    </Box>
+            </Container>
+            </React.Fragment>
+    );
+}
+
+
