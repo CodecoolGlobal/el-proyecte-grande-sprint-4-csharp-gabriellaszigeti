6dc5824a4273120b957b31b9420615d88311e3f6
gabriellaszigeti <gbszgt@gmail.com>
2021-10-28 08:39:49 +0100

    feat : start impelemnt discover view page

---
+++ el proyecte grande sprint 1/project-lens/src/components/DiscoverView.js
@@ -0,0 +1,8 @@
+export default function DiscoverView() {
+    return (
+        <div className="images-center">
+            <article class="image-container">
+            </article>
+        </div>
+    )
+}
