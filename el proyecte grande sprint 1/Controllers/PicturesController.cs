76af8e0d018d3a905e45711bd5b6068dc8c56045
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-10 17:24:02 +0100

    feat: Connect to blob storage and upload image

--- el proyecte grande sprint 1/Controllers/PicturesController.cs
+++ el proyecte grande sprint 1/Controllers/PicturesController.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using el_proyecte_grande_sprint_1.Models.DTO;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Azure.Storage.Blobs;
 
 namespace el_proyecte_grande_sprint_1.Controllers
 {
@@ -38,9 +42,27 @@ namespace el_proyecte_grande_sprint_1.Controllers
 
 
         [HttpPost]
-        public IActionResult CreateImage([FromForm] ImageDTO img)
+        public async Task<ActionResult>CreateImage([FromForm] ImageDTO img)
         {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=projectlens;AccountKey=9ras80E5iOB1hxIXVm00ew+bY42Pp9BQEf4kcPwqQG59OPQ6FLcr1uPu0q/6DLJn5Djld2cHr4JSlx6WE8bYBQ==;EndpointSuffix=core.windows.net";
+            using var memoryStream = new MemoryStream();
+            await img.Image.CopyToAsync(memoryStream);
+            //using var imageToConvert = Image.FromStream(memoryStream);
+            //imageToConvert.Save("myfile.png", ImageFormat.Png);
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("projectlens-blob1");
+
+            BlobClient blobClient = containerClient.GetBlobClient(img.Image.FileName);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            await blobClient.UploadAsync(memoryStream, true);
+
+
+
             return Ok();
+
         }
 
     }
