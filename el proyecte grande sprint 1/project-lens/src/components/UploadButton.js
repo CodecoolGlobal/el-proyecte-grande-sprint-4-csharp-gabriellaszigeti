cb4e83efed34811bf51d9bd79efd026458fcdb6c
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-11 12:38:05 +0100

    feat: Create Upload buttons and structure

--- el proyecte grande sprint 1/project-lens/src/components/UploadButton.js
+++ el proyecte grande sprint 1/project-lens/src/components/UploadButton.js
@@ -1,70 +1,52 @@
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import React, { useState } from 'react';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import React, { useState } from "react";
 
-const Input = styled('input')({
-  display: 'none',
+const Input = styled("input")({
+  display: "none",
 });
 
 export default function UploadButtons() {
-    const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
-    const changeHandler = (event) => {
-        setSelectedFile(event.target.files[0]);
-        setIsFilePicked(true);
-    };
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
 
-    const handleSubmission = () => {
-        const formData = new FormData();
+  const Input = styled('input')({
+    display: 'none',
+  });
 
-        formData.append('Image', selectedFile);
+  const handleSubmission = () => {
+    const formData = new FormData();
 
-        fetch(
-            'api/pictures',
-            {
-                method: 'POST',
-                body: formData,
-            }
-        )
-            .then((response) => response.json())
-            .then((result) => {
-                console.log('Success:', result);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    };
-    return (
- //       <div>
-	//	<input type="file" name="file" onChange={changeHandler} />
-	//	{isFilePicked ? (
-	//		<div>
-	//		</div>
-	//	) : (
-	//		<p>Select a file to show details</p>
-	//	)}
-	//	<div>
-	//		<button onClick={handleSubmission}>Submit</button>
-	//	</div>
-	//</div>
-        <Stack alignItems="center">
-            <div>
-            <label htmlFor="contained-button-file">
-              <Input accept="image/*" id="contained-button-file" multiple type="file" name="file" onChange={changeHandler} />
-                		{isFilePicked ? (
-			            <div>
-			            </div>
-		            ) : (
-			      <p>Select a file to show details</p>
-                )}
-                <div>
-              <Button variant="contained" style={{ backgroundColor: 'black', width: '20em', height: '3em' }} component="span" onClick={handleSubmission}>
+    formData.append("Image", selectedFile);
+
+    fetch("api/pictures", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  return (
+<div>
+        <label htmlFor="contained-button-file">
+        <Input accept="image/*" id="contained-button-file" type="file" name="file" onChange={changeHandler} />
+        <Button variant="contained" component="span" style={{ backgroundColor: 'black', width: '60%', height: '30%' }}>
           Upload
-                    </Button>
-                    </div>
-                </label>
-            </div>
-    </Stack>
-    );
-}
+        </Button>
+      </label>
+      <br/>
+			<Button variant="contained" style={{ backgroundColor: 'black', width: '60%', height: '30%' }} onClick={handleSubmission} >Submit</Button>
+  </div>
+	)
+};
+
