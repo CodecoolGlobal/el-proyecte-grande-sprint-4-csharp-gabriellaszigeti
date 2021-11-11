fba7ee407c90b63c7563e90888da3eefe79cf202
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-11 15:02:30 +0100

    feat: Create AzurePictureDTO

---
+++ el proyecte grande sprint 1/Models/DTO/AzurePictureDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Models.DTO
+{
+    public class AzurePictureDTO
+    {
+        public AzurePictureDTO(string url, string name, string title)
+        {
+            Url = url;
+            Name = name;
+            Title = title;
+        }
+
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
