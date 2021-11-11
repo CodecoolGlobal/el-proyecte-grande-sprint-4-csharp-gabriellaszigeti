09b5f5d47082d5d79552797136155ffa05e1f0f0
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-11 16:21:05 +0000

    refactor : add connectioString and bolbContainerName fields

--- el proyecte grande sprint 1/Controllers/PicturesController.cs
+++ el proyecte grande sprint 1/Controllers/PicturesController.cs
@@ -20,6 +20,9 @@ namespace el_proyecte_grande_sprint_1.Controllers
     {
 
         private readonly ILogger<PicturesController> _logger;
+        private string _connectionString = "DefaultEndpointsProtocol=https;AccountName=projectlens;AccountKey=9ras80E5iOB1hxIXVm00ew+bY42Pp9BQEf4kcPwqQG59OPQ6FLcr1uPu0q/6DLJn5Djld2cHr4JSlx6WE8bYBQ==;EndpointSuffix=core.windows.net";
+        private string _blobContainerName = "projectlens-blob1";
+
 
         private IPictureStorage _pictureStorage;
         public PicturesController(ILogger<PicturesController> logger, IPictureStorage pictureStorage)
@@ -43,41 +46,39 @@ namespace el_proyecte_grande_sprint_1.Controllers
 
 
         [HttpPost]
-        public async Task<ActionResult>CreateImage([FromForm] ImageDTO img)
+        public async Task<ActionResult> CreateImage([FromForm] ImageDTO img)
         {
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=projectlens;AccountKey=9ras80E5iOB1hxIXVm00ew+bY42Pp9BQEf4kcPwqQG59OPQ6FLcr1uPu0q/6DLJn5Djld2cHr4JSlx6WE8bYBQ==;EndpointSuffix=core.windows.net";
             using var memoryStream = new MemoryStream();
             await img.Image.CopyToAsync(memoryStream);
-            //using var imageToConvert = Image.FromStream(memoryStream);
-            //imageToConvert.Save("myfile.png", ImageFormat.Png);
 
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("projectlens-blob1");
-
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_blobContainerName);
             BlobClient blobClient = containerClient.GetBlobClient(img.Image.FileName);
 
             memoryStream.Seek(0, SeekOrigin.Begin);
+
             await blobClient.UploadAsync(memoryStream, true);
 
             return Ok();
         }
 
+
         [HttpGet]
         [Route("get-all-pictures")]
         public async Task<ActionResult<IEnumerable<AzurePictureDTO>>>GetAllPicturesFromBlobContainer()
         {
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=projectlens;AccountKey=9ras80E5iOB1hxIXVm00ew+bY42Pp9BQEf4kcPwqQG59OPQ6FLcr1uPu0q/6DLJn5Djld2cHr4JSlx6WE8bYBQ==;EndpointSuffix=core.windows.net";
-            var blobContainerClient = new BlobContainerClient(connectionString, "projectlens-blob1");
+            var blobContainerClient = new BlobContainerClient(_connectionString, _blobContainerName);
             List<AzurePictureDTO> azurePictures = new List<AzurePictureDTO>();
 
             await foreach (var blobItem in blobContainerClient.GetBlobsAsync())
             {
                 var blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
                 var uri = blobClient.Uri;
+
                 AzurePictureDTO azurePicture = new AzurePictureDTO(uri.OriginalString, blobItem.Name, blobItem.Name);
                 azurePictures.Add(azurePicture);
             }
+
             return Ok(azurePictures);
 
         }
