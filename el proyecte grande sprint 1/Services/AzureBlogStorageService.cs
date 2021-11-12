55e9afa4aff70f4c636e03dc44bd548982700fcb
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-12 12:46:03 +0100

    feat:  Create GetAllPicturesFromBlobContainer method

--- el proyecte grande sprint 1/Services/AzureBlogStorageService.cs
+++ el proyecte grande sprint 1/Services/AzureBlogStorageService.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using el_proyecte_grande_sprint_1.Models.DTO;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,27 @@ namespace el_proyecte_grande_sprint_1.Services
         {
             _configuration = configuration;
             _blobServiceClient = new BlobServiceClient(_configuration["AzureStorageConnectionString"]);
+        }
+
+
+        public async Task<IEnumerable<AzurePictureDTO>> GetAllPicturesFromBlobContainer(string container)
+        {
+            List<AzurePictureDTO> azurePictures = new List<AzurePictureDTO>();
+
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(container);
+
+            await foreach (var blobItem in blobContainerClient.GetBlobsAsync())
+            {
+                var blobClient = blobContainerClient.GetBlobClient(blobItem.Name);
+                var uri = blobClient.Uri;
+
+                AzurePictureDTO azurePicture = new AzurePictureDTO(uri.OriginalString, blobItem.Name, blobItem.Name);
+                azurePictures.Add(azurePicture);
+            }
+
+            return azurePictures;
+
+        }
         }
     }
 }
