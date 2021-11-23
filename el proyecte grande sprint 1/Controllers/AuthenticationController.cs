e2d377b67217fd57de2019603aacfc305587ca39
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 11:52:10 +0100

    feat: Create Authentication Controller

---
+++ el proyecte grande sprint 1/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Controllers
+{
+    public class AuthenticationController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
