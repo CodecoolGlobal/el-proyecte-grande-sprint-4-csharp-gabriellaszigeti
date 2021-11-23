4e9141b59851e15c44bf87d21789494b40f539b5
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 12:29:34 +0100

    feat: Set access modifier to public within User class in order to make it accessible for EFCore

--- el proyecte grande sprint 1/Models/User.cs
+++ el proyecte grande sprint 1/Models/User.cs
@@ -4,11 +4,10 @@ using System.Security.Cryptography;
 public class User {
 
 	[Key]
-	private int Id { get; set; }
-
-	private string Email { get; set; }
-	private string Username { get; set; }
-	private Rfc2898DeriveBytes Password { get; set;}
+	public int Id { get; set; }
+	public string Email { get; set; }
+	public string Username { get; set; }
+	public Rfc2898DeriveBytes Password { get; set;}
 
 	public Rfc2898DeriveBytes HashPassword(string password) {
 		throw new System.NotImplementedException("Not implemented");
