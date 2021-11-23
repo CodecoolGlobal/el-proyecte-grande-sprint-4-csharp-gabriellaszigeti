6ae2c8393ac581e8392f31c9f56cb648036acf6a
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 12:25:02 +0100

    feat: Create ctor in ApplicationContext

--- el proyecte grande sprint 1/Data/ApplicationContext.cs
+++ el proyecte grande sprint 1/Data/ApplicationContext.cs
@@ -4,6 +4,9 @@ namespace el_proyecte_grande_sprint_1.Data
 {
     public class ApplicationContext: DbContext
     {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
 
         public DbSet<User> Users { get; set; }
 
