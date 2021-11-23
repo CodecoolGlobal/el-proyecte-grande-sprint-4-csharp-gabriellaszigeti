8381aaf6b3787b249632890f791f5903efc5610a
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 17:15:17 +0100

    feat: Create Aunthentication Controller

--- el proyecte grande sprint 1/Controllers/AuthenticationController.cs
+++ el proyecte grande sprint 1/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using el_proyecte_grande_sprint_1.Repository;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +7,20 @@ using System.Threading.Tasks;
 
 namespace el_proyecte_grande_sprint_1.Controllers
 {
+    [Route("api/authentication")]
+    [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        public IActionResult Index()
+        private IAuthenticationRepository _authenticationRepository;
+
+        public AuthenticationController(IAuthenticationRepository authenticationRepository)
         {
+            _authenticationRepository = authenticationRepository;
+        }
+
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            await _authenticationRepository.AddUser(user);
             return Ok();
         }
     }
