b6ee40524368024256e313bf4282f736ef12c07b
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 12:39:01 +0100

    fix: Change Password Property type to string instead of Rfc2898DeriveBytes to allow EntityFramework mapping

--- el proyecte grande sprint 1/Models/User.cs
+++ el proyecte grande sprint 1/Models/User.cs
@@ -7,7 +7,7 @@ public class User {
 	public int Id { get; set; }
 	public string Email { get; set; }
 	public string Username { get; set; }
-	public Rfc2898DeriveBytes Password { get; set;}
+	public string Password { get; set;}
 
 	public Rfc2898DeriveBytes HashPassword(string password) {
 		throw new System.NotImplementedException("Not implemented");
