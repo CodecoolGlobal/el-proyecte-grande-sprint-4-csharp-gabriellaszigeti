8dcc70542b32ddc6c0d898ce31472edd8506cbae
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 12:21:30 +0100

    feat: Create ApplicationContext

---
+++ el proyecte grande sprint 1/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace el_proyecte_grande_sprint_1.Data
+{
+    public class ApplicationContext: DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
