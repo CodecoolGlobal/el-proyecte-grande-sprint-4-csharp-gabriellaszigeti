b94d86c07468eede17807cde619e60584fb40b8e
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 16:47:15 +0100

    feat: Create AuthenticationRepository

---
+++ el proyecte grande sprint 1/Repository/AuthenticationRepository.cs
@@ -0,0 +1,24 @@
+﻿using el_proyecte_grande_sprint_1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Repository
+{
+    public class AuthenticationRepository : IAuthenticationRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public AuthenticationRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUser(User user)
+        {
+            await _context.AddAsync(user);
+            _context.SaveChanges();
+        }
+    }
+}
