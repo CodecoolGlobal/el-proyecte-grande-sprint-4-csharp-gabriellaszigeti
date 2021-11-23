dd7caefa92fcc4f2b8d6011ed9b1e6cec0f59371
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-23 15:51:07 +0100

    feat: Implement HashPassword Method

--- el proyecte grande sprint 1/Models/User.cs
+++ el proyecte grande sprint 1/Models/User.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
 
@@ -9,8 +10,19 @@ public class User {
 	public string Username { get; set; }
 	public string Password { get; set;}
 
-	public Rfc2898DeriveBytes HashPassword(string password) {
-		throw new System.NotImplementedException("Not implemented");
+	public string HashPassword(string password) {
+
+		byte[] salt;
+		new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+		var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+		byte[] hash = pbkdf2.GetBytes(20);
+
+		byte[] hashBytes = new byte[36];
+		Array.Copy(salt, 0, hashBytes, 0, 16);
+		Array.Copy(hash, 0, hashBytes, 16, 20);
+
+		return Convert.ToBase64String(hashBytes);
 	}
 	public void ChangePassword(string newPassword) {
 		throw new System.NotImplementedException("Not implemented");
