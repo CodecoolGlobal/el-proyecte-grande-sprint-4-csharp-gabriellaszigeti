792386db0b25fbaedd7bdd737c1a48889184f081
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-24 13:45:09 +0000

    chore : add CheckIfUserAlreadyRegistered method to interface

--- el proyecte grande sprint 1/Repository/IAuthenticationRepository.cs
+++ el proyecte grande sprint 1/Repository/IAuthenticationRepository.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using el_proyecte_grande_sprint_1.Models.DTO;
+using System.Threading.Tasks;
 
 namespace el_proyecte_grande_sprint_1.Repository
 {
     public interface IAuthenticationRepository
     {
         Task AddUser(User user);
+        Task<bool> CheckIfUserAlreadyRegistered(UserDTO partialUserData);
     }
 }
