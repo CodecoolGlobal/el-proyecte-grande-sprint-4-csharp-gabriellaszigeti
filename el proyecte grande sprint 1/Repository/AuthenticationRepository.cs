9a0f6bcb251f9b990589b42af510968a2608fa9e
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-24 13:44:50 +0000

    feat : implement CheckIfUserAlreadyRegistered method

--- el proyecte grande sprint 1/Repository/AuthenticationRepository.cs
+++ el proyecte grande sprint 1/Repository/AuthenticationRepository.cs
@@ -1,5 +1,7 @@
 ﻿using el_proyecte_grande_sprint_1.Data;
+using el_proyecte_grande_sprint_1.Models.DTO;
 using el_proyecte_grande_sprint_1.Utils;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +24,12 @@ namespace el_proyecte_grande_sprint_1.Repository
             user.Password = _utility.HashPassword(user.Password);
             await _context.AddAsync(user);
             _context.SaveChanges();
+        }
+
+        public async Task<bool> CheckIfUserAlreadyRegistered(UserDTO partialUserData)
+        {
+            return await  _context.Users.AnyAsync(user => user.Email == partialUserData.Email || user.Username == partialUserData.Username);
+
         }
     }
 }
