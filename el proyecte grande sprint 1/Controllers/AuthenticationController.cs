9a0d64aaf334047f5c84404f58b430542d473afc
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-24 13:43:59 +0000

    feat : implement user validation method to check if user is already registered

--- el proyecte grande sprint 1/Controllers/AuthenticationController.cs
+++ el proyecte grande sprint 1/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using el_proyecte_grande_sprint_1.Repository;
+﻿using el_proyecte_grande_sprint_1.Models.DTO;
+using el_proyecte_grande_sprint_1.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,10 +19,21 @@ namespace el_proyecte_grande_sprint_1.Controllers
             _authenticationRepository = authenticationRepository;
         }
 
+        [HttpPost]
         public async Task<IActionResult> AddUser([FromBody] User user)
         {
             await _authenticationRepository.AddUser(user);
             return Ok();
         }
+
+        [HttpPost]
+        [Route("user-validation")]
+        public async Task<ActionResult<string>> CheckIfUserIsRegistered([FromBody] UserDTO partialUserData)
+        {
+            var result = await _authenticationRepository.CheckIfUserAlreadyRegistered(partialUserData);
+            return Ok(result.ToString());
+        }
+
+
     }
 }
