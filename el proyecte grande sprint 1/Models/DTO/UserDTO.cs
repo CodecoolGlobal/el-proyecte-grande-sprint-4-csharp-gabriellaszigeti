15b3d8b3c6dd6b242a2f8e0ee5baf2c5fcbc7121
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-24 13:44:18 +0000

    feat : add UserDTO class and it's properties

---
+++ el proyecte grande sprint 1/Models/DTO/UserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Models.DTO
+{
+    public class UserDTO
+    {
+        public UserDTO()
+        {
+        }
+
+        public UserDTO(string email, string name)
+        {
+            Email = email;
+            Username = name;
+        }
+
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
