e7a5094bd1ee19bc1fd928cd22c5faf1c62dbd1d
Gabriella Szigeti <gbszgt@gmail.com>
2021-11-24 12:11:19 +0000

    fix : remove HashPassword method from User class

--- el proyecte grande sprint 1/Models/User.cs
+++ el proyecte grande sprint 1/Models/User.cs
@@ -1,7 +1,5 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography;
-
 public class User {
 
 	[Key]
@@ -10,22 +8,8 @@ public class User {
 	public string Username { get; set; }
 	public string Password { get; set;}
 
-	public string HashPassword(string password) {
-
-		byte[] salt;
-		new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
-
-		var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
-		byte[] hash = pbkdf2.GetBytes(20);
-
-		byte[] hashBytes = new byte[36];
-		Array.Copy(salt, 0, hashBytes, 0, 16);
-		Array.Copy(hash, 0, hashBytes, 16, 20);
-
-		return Convert.ToBase64String(hashBytes);
-	}
-	public void ChangePassword(string newPassword) {
+	public void ChangePassword(string newPassword)
+	{
 		throw new System.NotImplementedException("Not implemented");
 	}
-
 }
