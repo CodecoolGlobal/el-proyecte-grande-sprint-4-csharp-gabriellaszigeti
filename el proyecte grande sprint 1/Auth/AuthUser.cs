8c3ad4f62486e038897bca2d29f1a0bd2d4aa6ae
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-25 13:35:42 +0100

    feat: Create AuthUser class

---
+++ el proyecte grande sprint 1/Auth/AuthUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Auth
+{
+    public static class AuthUser
+    {
+        public static string ServiceToken = "";
+    }
+}
