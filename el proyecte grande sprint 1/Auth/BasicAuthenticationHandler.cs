748f3fb6e5b19da9b5b9b5a6e1084058dd1fa315
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-25 13:36:15 +0100

    feat: Create BasicAuthenticationHandler class

---
+++ el proyecte grande sprint 1/Auth/BasicAuthenticationHandler.cs
@@ -0,0 +1,64 @@
+﻿using el_proyecte_grande_sprint_1.Repository;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Auth
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly IAuthenticationRepository _authenticationRepository;
+
+        public BasicAuthenticationHandler(IAuthenticationRepository authenticationRepository)
+        {
+            _authenticationRepository = authenticationRepository;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            // skip authentication if endpoint has [AllowAnonymous] or [AuthorizeWithTokenAttribute] attribute
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null || endpoint?.Metadata?.GetMetadata<AuthorizeWithTokenAttribute>() != null)
+                return AuthenticateResult.NoResult();
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            User user = null;
+            try
+            {
+                var authorizationHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authorizationHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var username = credentials[0];
+                var password = credentials[1];
+                user = await _authenticationRepository.FindUserByUsername(username);
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Error Occured.Authorization failed.");
+            }
+            if (user == null)
+                return AuthenticateResult.Fail("Invalid Credentials");
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
