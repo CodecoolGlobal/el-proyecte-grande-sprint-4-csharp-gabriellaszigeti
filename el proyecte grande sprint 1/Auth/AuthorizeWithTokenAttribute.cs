e3eb31123b00fd82800e764fd6de2fa51f73b536
cc.kprohaszka@gmail.com <cc.kprohaszka@gmail.com>
2021-11-25 13:35:59 +0100

    feat: Create AuthorizeWithTokenAttribute class

---
+++ el proyecte grande sprint 1/Auth/AuthorizeWithTokenAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace el_proyecte_grande_sprint_1.Auth
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeWithTokenAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            if (token == null || !token.Equals(AuthUser.ServiceToken))
+            {
+                // not logged in
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
